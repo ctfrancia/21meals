61813c180f083c7253d5cb5a2143cd071ac4dab1
Christian Francia <CTFrancia@gmail.com>
2019-01-18 15:30:30 +0100

    add material ui

--- twentyone-meals/src/app/app.module.ts
+++ twentyone-meals/src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
+
+// const appRoutes: Routes = [
+//   { path: 'signup', component: SignupComponent },
+//   { path: 'login', component: LoginComponent},
+//   { path: '',
+//     redirectTo: '/login',
+//     pathMatch: 'full'
+//   }
+// ];
 
 @NgModule({
   declarations: [
@@ -14,6 +24,10 @@ import { SignupComponent } from './signup/signup.component';
     SignupComponent
   ],
   imports: [
+    // RouterModule.forRoot(
+    //   appRoutes,
+    //   {enableTracing: true}
+    // ),
     BrowserModule,
     AppRoutingModule
   ],
