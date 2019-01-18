de42127c43c646dff4fafa41857d89498b97ac59
Christian Francia <CTFrancia@gmail.com>
2019-01-18 18:20:33 +0100

    add material ui

--- twentyone-meals/src/app/app.module.ts
+++ twentyone-meals/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MyMaterialModule } from '../material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +15,7 @@ import { SignupComponent } from './signup/signup.component';
     SignupComponent
   ],
   imports: [
+    MyMaterialModule,
     BrowserModule,
     AppRoutingModule
   ],
---
+++ twentyone-meals/src/material.module.ts
@@ -0,0 +1,62 @@
+import { NgModule } from '@angular/core';
+import {
+  MatAutocompleteModule,
+  MatButtonToggleModule,
+  MatButtonModule,
+  MatCheckboxModule,
+  MatDialogModule,
+  MatListModule,
+  MatGridListModule,
+  MatCardModule,
+  MatChipsModule,
+  MatIconModule,
+  MatInputModule,
+  MatMenuModule,
+  MatProgressSpinnerModule,
+  MatProgressBarModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSlideToggleModule,
+  MatSliderModule,
+  MatSidenavModule,
+  MatSnackBarModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const modules = [
+  MatAutocompleteModule,
+  MatButtonToggleModule,
+  MatButtonModule,
+  MatCheckboxModule,
+  MatDialogModule,
+  MatListModule,
+  MatGridListModule,
+  MatCardModule,
+  MatChipsModule,
+  MatIconModule,
+  MatInputModule,
+  MatMenuModule,
+  MatProgressSpinnerModule,
+  MatProgressBarModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSlideToggleModule,
+  MatSliderModule,
+  MatSidenavModule,
+  MatSnackBarModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  BrowserAnimationsModule,
+];
+
+@NgModule({
+  imports: modules,
+  exports: modules,
+  })
+export class MyMaterialModule {}
